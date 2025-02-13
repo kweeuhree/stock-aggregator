dd504255794e806cec4c557ff71828717dae99a3
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-13 10:00:20 -0500

    test(fetch): add test for fetch method

---
+++ internal/fetcher/fetch_test.go
@@ -0,0 +1,65 @@
+package fetcher
+
+import (
+	"log"
+	"testing"
+)
+
+type MockRequesterParser struct {
+	MockRequest func(url string) ([]map[string]interface{}, error)
+}
+
+func (mrp *MockRequesterParser) RequestAndParse(url string) ([]map[string]interface{}, error) {
+	return mrp.MockRequest(url)
+}
+
+type MyMockRequesterParser struct{}
+
+type MockFetcher struct {
+	ErrorLog        *log.Logger
+	Urls            *[]string
+	RequesterParser MockRequesterParser
+}
+
+func TestFetch(t *testing.T) {
+	tests := []struct {
+		name        string
+		testUrls    []string
+		mockRequest func(url string) ([]map[string]interface{}, error)
+		expectError bool
+	}{
+		{
+			name:     "Successful fetch",
+			testUrls: []string{"https://i-am-url.com", "https://i-am-stock-api.com", "https://go-dev.go"},
+			mockRequest: func(url string) ([]map[string]interface{}, error) {
+				return []map[string]interface{}{
+					{"symbol": "AAPL"},
+				}, nil
+			},
+			expectError: false,
+		},
+	}
+
+	for _, entry := range tests {
+		t.Run(entry.name, func(t *testing.T) {
+			mockRequesterParser := &MockRequesterParser{
+				MockRequest: entry.mockRequest,
+			}
+
+			fetcher := &Fetcher{
+				ErrorLog:        log.Default(),
+				Urls:            entry.testUrls,
+				RequesterParser: mockRequesterParser,
+			}
+
+			data, err := fetcher.Fetch()
+			if (err != nil) != entry.expectError {
+				t.Errorf("expected error: %v, got: %v", entry.expectError, err)
+			}
+
+			if err == nil && len(data) == 0 {
+				t.Errorf("expected data, got none")
+			}
+		})
+	}
+}
