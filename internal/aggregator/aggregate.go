4a3e3d10f6361ed86f0f321fb65af488636ec856
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-13 09:53:30 -0500

    refactor(aggregate): Aggregate method has its own file to improve readability

--- internal/aggregator/aggregate.go
+++ internal/aggregator/aggregate.go
@@ -1,6 +1,17 @@
 package aggregator
 
-func (a *Aggregator) Aggregate([]map[string]interface{}) error {
-	a.InfoLog.Println("Attempting to aggregate...")
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func (a *Aggregator) Aggregate(data []map[string]interface{}) error {
+	// Pretty print JSON
+	prettyJSON, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		a.ErrorLog.Println("Failed to format JSON:", err)
+	} else {
+		fmt.Println(string(prettyJSON))
+	}
 	return nil
 }
