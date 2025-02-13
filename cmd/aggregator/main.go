3dbe42b98ee0e39c3f2da6e7917bfe0c25136764
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-13 09:52:36 -0500

    refactor(main): break down the main function into main and run to improve readability and testability

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -5,39 +5,50 @@ import (
 	"os"
 	"stock-aggregator/internal/aggregator"
 	"stock-aggregator/internal/fetcher"
-
-	"github.com/joho/godotenv"
 )
 
 type application struct {
-	infoLog    *log.Logger
 	errorLog   *log.Logger
 	fetcher    *fetcher.Fetcher
 	aggregator *aggregator.Aggregator
 }
 
 func main() {
-	godotenv.Load()
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-	fetcher := fetcher.New(infoLog, errorLog)
 
-	aggregator := aggregator.New(infoLog, errorLog)
+	urls, err := getUrls()
+	if err != nil {
+		errorLog.Fatalf("failed getting urls: %s", err)
+	}
+
+	requester := &fetcher.Requester{}
+	fetcher := fetcher.New(requester, errorLog, urls)
+	aggregator := aggregator.New(errorLog)
 
 	app := application{
-		infoLog:    infoLog,
 		errorLog:   errorLog,
 		fetcher:    fetcher,
 		aggregator: aggregator,
 	}
 
+	if err := app.run(); err != nil {
+		errorLog.Fatal(err)
+	}
+}
+
+func (app *application) run() error {
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	infoLog.Println("Fetching data concurrently...")
 	fetched, err := app.fetcher.Fetch()
 	if err != nil {
-		app.errorLog.Printf("failed scraping: %+v", err)
+		app.errorLog.Printf("failed fetching: %+v", err)
 	}
 
 	err = app.aggregator.Aggregate(fetched)
 	if err != nil {
 		app.errorLog.Printf("failed aggregating: %+v", err)
 	}
+
+	return nil
 }
