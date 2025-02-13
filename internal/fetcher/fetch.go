5a9aa46e64273b054486f4a32e904b9ae28a8d84
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-13 09:55:37 -0500

    refactor(fetch): fetch.go now houses fetching function exclusively

--- internal/fetcher/fetch.go
+++ internal/fetcher/fetch.go
@@ -1,70 +1,25 @@
 package fetcher
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 	"sync"
 )
 
 func (f *Fetcher) Fetch() ([]map[string]interface{}, error) {
-	f.InfoLog.Println("Attempting to fetch...")
-	urls, err := f.getUrls()
-	if err != nil {
-		return nil, err
-	}
-
-	data, err := f.fetchData(urls)
-	if err != nil {
-		f.ErrorLog.Fatal(err)
-	}
-
-	// Pretty print JSON
-	prettyJSON, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		f.ErrorLog.Println("Failed to format JSON:", err)
-	} else {
-		fmt.Println(string(prettyJSON))
-	}
-	return data, nil
-}
-
-func (f *Fetcher) getUrls() ([]string, error) {
-	marketStackApikey := os.Getenv("MARKET_STACK_API_KEY")
-	fmpCloudApiKey := os.Getenv("FMP_CLOUD_API_KEY")
-
-	if marketStackApikey == "" || fmpCloudApiKey == "" {
-		return nil, fmt.Errorf("empty api key")
-	}
-
-	stocks := "AAPL,MSFT"
-
-	// marketStackUrl := fmt.Sprintf("https://api.marketstack.com/v1/intraday/latest?access_key=%s&symbols=%s", marketStackApikey, stocks)
-
-	fmpCloudURl := fmt.Sprintf("https://fmpcloud.io/api/v3/quote/%s?apikey=%s", stocks, fmpCloudApiKey)
-
-	urls := []string{fmpCloudURl}
-
-	return urls, nil
-}
-
-func (f *Fetcher) fetchData(urls []string) ([]map[string]interface{}, error) {
-	f.InfoLog.Println("Fetching data concurrently...")
-
 	wg := sync.WaitGroup{}
 
-	dataChan := make(chan []map[string]interface{}, len(urls))
-	errChan := make(chan error, len(urls))
+	dataChan := make(chan []map[string]interface{}, len(f.Urls))
+	errChan := make(chan error, len(f.Urls))
 
-	for _, url := range urls {
+	for _, url := range f.Urls {
 		wg.Add(1)
 
 		go func(url string) {
 			defer wg.Done()
-			batch, err := f.RequestAndParse(url)
+			batch, err := f.RequesterParser.RequestAndParse(url)
 
 			if err != nil {
-				f.ErrorLog.Printf("failed fetching.\t\nurl: %s\t\nerr: %+v", url, err)
+				f.ErrorLog.Printf("\t\nurl: %s\t\nerr: %+v", url, err)
 				errChan <- err
 				return
 			}
