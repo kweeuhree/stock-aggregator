3db961169036967227002b17945058bbaed8bf53
Nika Kolesnikova <firstnamenika@gmail.com>
2025-02-20 14:53:38 -0500

    feat(getUrls): remove multiple stocks option and add FinHub api key and url

--- cmd/aggregator/getUrls.go
+++ cmd/aggregator/getUrls.go
@@ -7,23 +7,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func getUrls() ([]string, error) {
+func getUrls(stock string) ([]string, error) {
 	godotenv.Load()
 
 	marketStackApikey := os.Getenv("MARKET_STACK_API_KEY")
 	fmpCloudApiKey := os.Getenv("FMP_CLOUD_API_KEY")
+	finHubApiKey := os.Getenv("FINHUB_API_KEY")
 
 	if marketStackApikey == "" || fmpCloudApiKey == "" {
 		return nil, fmt.Errorf("empty api key")
 	}
 
-	stocks := "AAPL,MSFT"
+	marketStackUrl := fmt.Sprintf("https://api.marketstack.com/v1/eod/latest?access_key=%s&symbols=%s", marketStackApikey, stock)
+	fmpCloudURl := fmt.Sprintf("https://fmpcloud.io/api/v3/quote/%s?apikey=%s", stock, fmpCloudApiKey)
+	finHubUrl := fmt.Sprintf("https://finnhub.io/api/v1/quote?symbol=%s&token=%s", stock, finHubApiKey)
 
-	// marketStackUrl := fmt.Sprintf("https://api.marketstack.com/v1/intraday/latest?access_key=%s&symbols=%s", marketStackApikey, stocks)
-
-	fmpCloudURl := fmt.Sprintf("https://fmpcloud.io/api/v3/quote/%s?apikey=%s", stocks, fmpCloudApiKey)
-
-	urls := []string{fmpCloudURl}
+	urls := []string{marketStackUrl, finHubUrl, fmpCloudURl}
 
 	return urls, nil
 }
